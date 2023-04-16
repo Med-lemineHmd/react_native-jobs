c4a750e6da40fe6b20497f53a4efcb4923932f6f
Med-lemineHmd <leminlemin00@gmail.com>
2023-04-16 07:15:28 +0100

    Create Welcome Component

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,27 @@
-import React from "react";
+import { useCallback } from "react";
+import { Text, View } from "react-native";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const layout = () => {
-  return (
-    <View>
-      <Text>_layout</Text>
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default layout;
