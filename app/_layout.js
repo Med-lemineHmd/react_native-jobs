ad2fd79a6e93372bd5a894f5ba150175dd7621da
Med-lemineHmd <leminlemin00@gmail.com>
2023-04-16 05:26:45 +0100

    Project Setup

---
+++ app/_layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const layout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  );
+};
+
+export default layout;
