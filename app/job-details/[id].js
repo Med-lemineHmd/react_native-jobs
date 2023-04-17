4a30e37a3ca4f03fbe9dbcf8b9d8f74a34d19974
Med-lemineHmd <leminlemin00@gmail.com>
2023-04-17 15:13:52 +0100

    Search functionnality

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -44,12 +44,18 @@ const JobDetails = () => {
             points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
           />
         );
-        break;
       case "About":
-        break;
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
 
-      case "Responsabilities":
-        break;
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
 
       default:
         break;
@@ -111,6 +117,13 @@ const JobDetails = () => {
             </View>
           )}
         </ScrollView>
+
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results"
+          }
+        />
       </>
     </SafeAreaView>
   );
